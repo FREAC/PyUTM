183ce81466ddc60e5a7fcc8c490100cc009f4c54
pgriffith <philip.griffith@gmail.com>
2018-10-25 15:05:26 -0400

    Fixed version issue

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
+import os
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 version = {}
-with open('...sample/version.py') as fp:
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'pyutm', '__version__.py')) as fp:
     exec(fp.read(), version)
 
 setuptools.setup(
